62c35acbca2fed650df2349f958ee1148eac1af4
flurbudurbur <69259138+flurbudurbur@users.noreply.github.com>
2025-12-18 22:29:24 +0100

    cleaned up

--- home.nix
+++ home.nix
@@ -3,12 +3,27 @@
 {
 	home.username = "flur";
 	home.homeDirectory = "/home/flur";
-	programs.git.enable = true;
+	programs.git = {
+		enable = true;
+		userEmail = "69259138+flurbudurbur@users.noreply.github.com";
+		userName = "flurbudurbur";
+		extraConfig = {
+			init.defaultBranch = "main";
+		};
+	};
 	home.stateVersion = "25.11";
 	programs.bash = {
 		enable = true;
 		shellAliases = {
 			btw = "echo I use Nixos, btw";
+		};
+	};
+	programs.ssh = {
+		enable = true;
+		matchBlocks = {
+			"github.com" = {
+				identityFile = "~/.ssh/github";
+			};
 		};
 	};
 }
