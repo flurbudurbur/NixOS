55cde0afeef9ea87678ebb1071714bd611914d12
flurbudurbur <69259138+flurbudurbur@users.noreply.github.com>
2026-06-21 15:03:25 +0200

    hyprland tablet fix and nix fmt

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -1,22 +1,32 @@
 let
   requiredAttrs = [
-    "bg" "bg_alt" "bg_select"
-    "fg_faint" "fg_dim" "fg"
-    "error" "warning"
-    "accent2" "blue" "cyan" "accent"
-    "hl_low" "hl_med" "hl_high"
+    "bg"
+    "bg_alt"
+    "bg_select"
+    "fg_faint"
+    "fg_dim"
+    "fg"
+    "error"
+    "warning"
+    "accent2"
+    "blue"
+    "cyan"
+    "accent"
+    "hl_low"
+    "hl_med"
+    "hl_high"
   ];
 
   dir = builtins.readDir ./.;
 
-  themeFiles = builtins.filter
-    (name: name != "default.nix" && builtins.match ".*\\.nix" name != null)
-    (builtins.attrNames dir);
+  themeFiles = builtins.filter (
+    name: name != "default.nix" && builtins.match ".*\\.nix" name != null
+  ) (builtins.attrNames dir);
 
-  themeName = fileName:
-    builtins.substring 0 (builtins.stringLength fileName - 4) fileName;
+  themeName = fileName: builtins.substring 0 (builtins.stringLength fileName - 4) fileName;
 
-  validate = name: theme:
+  validate =
+    name: theme:
     let
       missing = builtins.filter (attr: !(builtins.hasAttr attr theme)) requiredAttrs;
     in
