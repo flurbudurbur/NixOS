ab373fdd95bef649007f344ec0652d5247a6b64e
flurbudurbur <69259138+flurbudurbur@users.noreply.github.com>
2026-07-01 19:10:15 +0200

    make widget full width

--- users/flur/wayland/eww/progress-module.nix
+++ users/flur/wayland/eww/progress-module.nix
@@ -9,28 +9,33 @@
         (box
           :class {class + " module"}
           :orientation "v"
-          :halign "center"
+          :halign "fill"
+          :hexpand true
           :valign "center"
-          :hexpand false
-          :vexpand false
-          :space-evenly false
-          :spacing 2
-          (revealer
-            :transition "slideup"
-            :duration "150ms"
-            :reveal {hover}
-            (label :class {class + "-value"} :text {value + "%"} :halign "center"))
-          (circular-progress
-            :class {class + "-progress"}
-            :value {value}
-            :thickness 3
-            :start-at 75
-            :clockwise true
-            :width 32
-            :height 32
+          (box
+            :orientation "v"
             :halign "center"
             :valign "center"
-            (label :text {icon} :halign "center" :valign "center")))))
+            :hexpand false
+            :vexpand false
+            :space-evenly false
+            :spacing 2
+            (revealer
+              :transition "slideup"
+              :duration "150ms"
+              :reveal {hover}
+              (label :class {class + "-value"} :text {value + "%"} :halign "center"))
+            (circular-progress
+              :class {class + "-progress"}
+              :value {value}
+              :thickness 3
+              :start-at 75
+              :clockwise true
+              :width 32
+              :height 32
+              :halign "center"
+              :valign "center"
+              (label :text {icon} :halign "center" :valign "center"))))))
   '';
 
   scss = ''
