fb474ef8767cca5b3ad488300f8d7dd18869200a
flurbudurbur <69259138+flurbudurbur@users.noreply.github.com>
2026-07-04 15:36:44 +0200

    wire up themes so that icons can be added. made it future-proof

---
+++ modules/themes/themes.nix
@@ -0,0 +1,22 @@
+[
+  {
+    theme = "rose-pine-moon";
+    icon = "";
+  }
+  {
+    theme = "catppuccin-mocha";
+    icon = "";
+  }
+  {
+    theme = "sakura";
+    icon = "";
+  }
+  {
+    theme = "gruvbox-dark";
+    icon = "";
+  }
+  {
+    theme = "sweet";
+    icon = "";
+  }
+]
